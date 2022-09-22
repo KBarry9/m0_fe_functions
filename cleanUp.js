c3508f5e6c37cb7df399fb27dcf07b8046e94547
Kelly Barry <kleemuir@gmail.com>
2022-09-22 16:29:53 -0600

    Initial commit

--- cleanUp.js
+++ cleanUp.js
@@ -3,44 +3,46 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
-
+nameQuestion();
+// I added semi colons after the parantheses on line 12 and line 15 to follow the format.
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+  var sum = first + second + third;
+  console.log(sum);
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
-
+// I tabbed over line 20 and 21, added a semi colon to line 20, and moved the curly brace in line 22 forward to fit the format.
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+}
 
 makeFreshPesto();
-
+// I spelled out function, added a space in line 29 between the end parantheses and the curly brace, then I moved the end curly bracket down to line 35 to follow format.
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
-
-  console.log(`the average is: ${avg}`.)
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  console.log(`the average is: ${avg}.`);
   }
+
+average (4, 10);
+// I moved the opening curly bracket to line 41 , then I tabbed the var in line 42 and 43, then I moved the console log up a line and added a semi colon, I moved the period inside the back tick, and added a call function.
